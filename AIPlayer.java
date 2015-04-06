6ea01b61253415657021478af9a24d9571c85a99
Rebecca Ansems <ansems_rebecca@hotmail.com>
2015-04-06 11:33:59 -0300

    Update AIPlayer.java

--- AIPlayer.java
+++ AIPlayer.java
@@ -1,19 +1,21 @@
-public class AIPlayer{
+public class AIPlayer {
 
-   public String[][] ships = new String[3][4];
+	public String[][] ships = new String[3][4];
 
-   public AIPlayer(){}
-      
-   public String getShips(int i, int j){
-      ships[0][0]="A1";
-      ships[0][1]="B1";
-      ships[0][2]="C1";
-      ships[0][3]="D1";
-      ships[1][0]="A2";
-      ships[1][1]="A3";
-      ships[1][2]="A4";
-      ships[2][0]="A5";
-      ships[2][1]="B5";
-      return ships[i][j];}
+	public AIPlayer() {
+		ships[0][0] = "A1";
+		ships[0][1] = "B1";
+		ships[0][2] = "C1";
+		ships[0][3] = "D1";
+		ships[1][0] = "B7";
+		ships[1][1] = "C7";
+		ships[1][2] = "D7";
+		ships[2][0] = "A5";
+		ships[2][1] = "A6";
+	}
+
+	public String getShips(int i, int j) {
+		return ships[i][j];
+	}
 
 }
